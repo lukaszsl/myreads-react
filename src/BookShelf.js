716971992a974d4a2b64d955c7cc4a5f3f9c7829
lukaszsl <lukasz.sliczner@interia.pl>
2018-07-15 23:33:19 +0000

    fix: Fix option settings on BookShelf component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,12 +3,38 @@ import React from 'react'
 class BookShelf extends React.Component {
 
 state = {
-	shelfName: this.props.shelfStatus
+	shelfName: this.props.shelfStatus,
+	option: [
+		{
+			"value": "move",
+			"text": "Move to...",
+			"key": "move"
+		},
+		{
+			"value": "currentlyReading",
+			"text": "Currently Reading",
+			"key": "currentlyReading"
+		},
+		{
+			"value": "wantToRead",
+			"text": "Want to Read",
+			"key": "wantToRead"
+		},
+		{
+			"value": "read",
+			"text": "Read",
+			"key": "read"
+		},
+		{
+			"value": "none",
+			"text": "None",
+			"key": "none"
+		},
+	]
 }
 
 	render() {
-
-const shelfName = this.state.shelfName
+		const shelfName = this.state.shelfName
 		return (
 			<div className="bookshelf">
 				<h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
@@ -22,8 +48,8 @@ const shelfName = this.state.shelfName
 									<div className="book-top">
 										<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
 										<div className="book-shelf-changer">
-											<select onChange={(e) => (this.props.onchangeBookState(book, e.target.value))}>
-												<option value="move" disabled>Move to...</option>
+											<select value={book.shelf} onChange={(e) => (this.props.onchangeBookState(book, e.target.value))}>
+												<option value= "move" disabled>{this.state.option[0].text}</option>
 												<option value="currentlyReading">Currently Reading</option>
 												<option value="wantToRead">Want to Read</option>
 												<option value="read">Read</option>
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -55,7 +55,7 @@ class SearchBooks extends React.Component {
 									<div className="book-top">
 										<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
 										<div className="book-shelf-changer">
-											<select onChange={(e) => (this.props.onchangeBookState(book, e.target.value))}>
+											<select value={book.shelf} onChange={(e) => (this.props.onchangeBookState(book, e.target.value))}>
 												<option value="move" disabled>Move to...</option>
 												<option value="currentlyReading">Currently Reading</option>
 												<option value="wantToRead">Want to Read</option>
